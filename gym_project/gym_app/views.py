628850447859b6964b07047e0277748dc95d6b03
alekmiltenov <162902525+alekmiltenov@users.noreply.github.com>
2024-03-14 13:31:25 +0200

    Update views.py

--- gym_project/gym_app/views.py
+++ gym_project/gym_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>home</h1>')
+    return render(request,'gym_app/home.html')
 
 def about(request):
-    return HttpResponse('<h1>About page</h1>')
+   return render(request,'gym_app/about.html')
