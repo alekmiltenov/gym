c6d690189a264aaef5951ddb8f3c4913a987868c
hristobelchinov <162201580+hristobelchinov@users.noreply.github.com>
2024-03-14 13:17:06 +0200

    login

--- gym_project/gym_register/views.py
+++ gym_project/gym_register/views.py
@@ -1,2 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth import 
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to login')
+            return redirect('gym_app-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'gym_register/register.html', {'form': form})
+
+
