e30391475bdec0abd3a80888f0aaf71bd764741e
alekmiltenov <162902525+alekmiltenov@users.noreply.github.com>
2024-03-14 11:01:50 +0200

    Gym register commit

--- gym_project/gym_project/settings.py
+++ gym_project/gym_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'gym_register.apps.GymRegisterConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
