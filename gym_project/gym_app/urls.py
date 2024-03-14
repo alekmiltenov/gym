8407bc4e8187c3556ddbf05af57988ba8ea73a6c
alekmiltenov <162902525+alekmiltenov@users.noreply.github.com>
2024-03-15 00:28:04 +0200

    about function

--- gym_project/gym_app/urls.py
+++ gym_project/gym_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . views import home
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+
 ]
