f9fbf34bcf62d2a7ff902877019864052980cba2
alekmiltenov <162902525+alekmiltenov@users.noreply.github.com>
2024-03-14 13:38:14 +0200

    Update urls.py

--- gym_project/gym_app/urls.py
+++ gym_project/gym_app/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from . import views
 
+#### test data
+posts = [
+    {
+        'author':'Alek',
+        'title' : 'This is the title',
+        'content':'This is the dummy data context',
+        'date_posted':'today',
+    }
+]
+
+
 urlpatterns = [
     path('', views.home,name="home"),
     path('about/', views.about,name="about"),
