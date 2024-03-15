914a2d2fbab7fa0bd67b998c9e9afec93835c4f4
alekmiltenov <162902525+alekmiltenov@users.noreply.github.com>
2024-03-15 09:11:02 +0200

    help me

--- gym_project/gym_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . views import home
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-
-]
--- gym_project/gym_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'gym_app/index.html')
-
-def about(request):
-    return render(request, 'gym_app/about.html')
-
-
