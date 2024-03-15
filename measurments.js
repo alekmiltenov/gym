a982fbfe429d1ad46c6382556e3e6e6fbaa48fb3
Martin-R-D <159622261+Martin-R-D@users.noreply.github.com>
2024-03-15 09:54:22 +0200

    New js file
    added js file for background-colors 'onclick' of inputs

---
+++ measurments.js
@@ -0,0 +1,76 @@
+//obliques functino
+var obliquesEl = document.getElementById('obliques');
+
+function onObliquesClick() {
+    document.getElementById('obliques_input').style.backgroundColor = 'red';
+}
+
+//forearms function
+
+var forearmsEl = document.getElementById('forearms');
+
+function onForearmsClick() {
+    document.getElementById('forearms_input').style.backgroundColor = 'red';
+}
+
+
+//forearms function
+
+var forearmsEl = document.getElementById('forearms');
+
+function onForearmsClick() {
+    document.getElementById('forearms_input').style.backgroundColor = 'red';
+}
+
+
+
+//biceps function
+
+var bicepsEl = document.getElementById('biceps');
+
+function onBicepsClick() {
+    document.getElementById('biceps_input').style.backgroundColor = 'red';
+}
+
+//traps function
+
+var trapsEl = document.getElementById('traps');
+
+function onTrapsClick() {
+    document.getElementById('traps_input').style.backgroundColor = 'red';
+}
+
+
+//forearms function
+
+var forearmsEl = document.getElementById('forearms');
+
+function onForearmsClick() {
+    document.getElementById('forearms_input').style.backgroundColor = 'red';
+}
+
+//chest function
+
+var chestEl = document.getElementById('chest');
+
+function onChestClick() {
+    document.getElementById('chest_input').style.backgroundColor = 'red';
+}
+
+
+//quads function
+
+var quadsEl = document.getElementById('quads');
+
+function onQuadsClick() {
+    document.getElementById('quads_input').style.backgroundColor = 'red';
+}
+
+//calves function
+
+var calvesEl = document.getElementById('calves');
+
+function onCalvesClick() {
+    document.getElementById('calves_input').style.backgroundColor = 'red';
+}
+
