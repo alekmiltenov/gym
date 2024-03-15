32d5dcabb01b2182eb74590dea0a177014e30e48
Martin-R-D <159622261+Martin-R-D@users.noreply.github.com>
2024-03-15 20:16:07 +0200

    added chart to first h1

--- stats_muscles.js
+++ stats_muscles.js
@@ -1,1 +1,42 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.6.0/chart.js"></script>
+
+
+let labels = ['mucsle','mucsle1','mucsle2','mucsle3','mucsle4']
+
+    let mucsleData = [0,50,88,107,50];
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            data: mucsleData,
+            borderColor: 'rgb(68,221,245)',
+            fill: true,
+            backgroundColor: 'rgb(134, 176, 246)',
+            tension: 0.1,
+            hoverBorderColor: 'red'
+        }]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+        options: {
+            plugins: {
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: 'Progres'
+                }
+            }
+        }
+    };
+
+    const chart = new Chart(
+        document.getElementById('abdominals_chart'),
+        config
+    );
+    const chart1 = new Chart(
+        document.getElementById('abdominals_chart'),
+        config
+    );
