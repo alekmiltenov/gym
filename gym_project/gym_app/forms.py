b6907fa4d6466827e34b1df148058267b0199f25
hristobelchinov <162201580+hristobelchinov@users.noreply.github.com>
2024-03-15 16:04:56 +0200

    updated forms.py

--- gym_project/gym_app/forms.py
+++ gym_project/gym_app/forms.py
@@ -3,6 +3,3 @@ from django import forms
 class DataForm(forms.Form):
     data = forms.CharField(widget=forms.HiddenInput())  # Hidden field to store JSON data
 
-class Meta:
-        model = MuscleMeasurement
-        exclude = ['muscle', 'measurement']  # Exclude fields you don't want
